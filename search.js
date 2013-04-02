7402e19b1da0c680d21ea1e92d30a154fc8f22f5
David Gailey <dave.g.github@gmail.com>
2013-04-02 15:16:20 -0400

    working with OAuth
    may move to private to keep keys secure

--- search.js
+++ search.js
@@ -2,16 +2,40 @@
 var request = require('superagent');
     
 module.exports = function search(query, fn) {
+	var TwitterOAuthHeader = 'OAuth ';
+		
+		TwitterOAuthHeader += encodeURIComponent('') + '="' + encodeURIComponent('') +'", ';
+		TwitterOAuthHeader += encodeURIComponent('') + '="' + encodeURIComponent('') +'", ';
+		TwitterOAuthHeader += encodeURIComponent('') + '="' + encodeURIComponent('') +'", ';
+		TwitterOAuthHeader += encodeURIComponent('') + '="' + encodeURIComponent('') +'", ';
+
+	/*
+		Authorization: 
+        OAuth oauth_consumer_key="xvz1evFS4wEEPTGEFPHBog", 
+              oauth_nonce="kYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg", 
+              oauth_signature="tnnArxj06cWHq44gCs1OSKk%2FjLY%3D", 
+              oauth_signature_method="HMAC-SHA1", 
+              oauth_timestamp="1318622958", 
+              oauth_token="370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb", 
+              oauth_version="1.0"
+
+
+		
 	
-	request.get('https://api.twitter.com/1.1/search/tweets.json',{q:query})
-	//.data({q:query})
-	.end(function(res){
-		console.log(res);
-		if(res.body && Array.isArray(res.body.results)){
-			return fn(null, res.body.results);
-		}
-		fn(new Error('bad twitter response'));
-	});
+
+	*/
+
+	request.get('http://search.twitter.com/search.json',{q:query})
+		.set('Authorization','')
+
+		//.data({q:query})
+		.end(function(res){
+			console.log(res);
+			if(res.body && Array.isArray(res.body.results)){
+				return fn(null, res.body.results);
+			}
+			fn(new Error('bad twitter response'));
+		});
 
 }
 
